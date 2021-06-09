f94e57eb576399f0aa91f241963e486ef186babb
Hedaya <khaledhedaya98@gmail.com>
2021-06-09 18:04:46 +0200

    Update prime number check.cpp

--- Math/prime number check.cpp
+++ Math/prime number check.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 const int N = 1e5 + 10, OO = 0x3f3f3f3f;
 
-bool isPrime(int n) {
+bool isPrime(int n) { // O(sqrt(n))
     if (n == 2) return true;
     if (n < 2 || !(n & 1)) return false;
     for (int i = 3; i * i <= n; i += 2) {
