b1252048fa3a5fc8c0094acd5af4ff85213acee4
Hedaya <khaledhedaya98@gmail.com>
2021-06-09 18:03:24 +0200

    Create prime number check.cpp

---
+++ Math/prime number check.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const int N = 1e5 + 10, OO = 0x3f3f3f3f;
+
+bool isPrime(int n) {
+    if (n == 2) return true;
+    if (n < 2 || !(n & 1)) return false;
+    for (int i = 3; i * i <= n; i += 2) {
+        if (n % i == 0) return false;
+    }
+    return true;
+}
+
+
+int main() {
+    
+    int x;
+    cin >> x;
+    if (isPrime(x)) cout << x << " is prime number\n";
+    else cout << "Not prime\n";
+
+
+    return 0;
+}
+
+
+
+
